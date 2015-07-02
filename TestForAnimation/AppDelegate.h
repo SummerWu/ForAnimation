561fa826638a78d654b7f47bc9294a49af4b8adf
DamonNG <DamonNG@DamonNGdeMacBook-Air.local>
2015-07-02 16:02:40 +0800

    Initial Commit

---
+++ TestForAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForAnimation
+//
+//  Created by DamonNG on 15/7/2.
+//  Copyright (c) 2015年 JiehaoWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestForAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestForAnimation
+//
+//  Created by DamonNG on 15/7/2.
+//  Copyright (c) 2015年 JiehaoWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
